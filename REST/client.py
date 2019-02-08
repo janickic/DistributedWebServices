35c35934dc4da427ac79eb612c87b9543cc64cef
Camille Janicki <camille.janicki@gmail.com>
2019-02-08 09:17:07 -0800

    Add comments to REST

--- REST/client.py
+++ REST/client.py
@@ -1,16 +1,23 @@
 import requests, json, sys, time
 
+# Check IP of server
 if len(sys.argv) < 2:
     print("Please insert IP of server")
     sys.exit()
-
 ip = sys.argv[1]
 print('Server ip:', ip)
+
+# Get time before remote call
 start = time.time()
+# Call the remote server
 response = requests.get("http://"+ip+":5000/time")
+# Parse out JSON reponse
 serverTime = response.json()["time"]
+# Get time after remote call
 end = time.time()
+# Calculate rtt
 rtt = (end - start) / 2
 updatedTime = serverTime + rtt
+# Output results
 print("Server time: ", serverTime)
 print("Updated time: ", updatedTime)
